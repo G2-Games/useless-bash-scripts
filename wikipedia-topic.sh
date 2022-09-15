fface206b21d330901f3e5a6dc386d9a0fe283a8
G2-Games <ke0bhogsg@gmail.com>
2022-09-15 09:36:56 -0500

    Cleaned up and reduced size significantly

--- wikipedia-topic.sh
+++ wikipedia-topic.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-
-source randcolor.sh
-
-topic=$1
-topic=$(echo $topic | tr " " '|')
+topic=$(echo $1 | sed -r 's/[ ,]+/|/g')
 echo $topic
 echo -n "Searching"
-
-while [[ $success -lt 1 ]]; do
+while ! echo "$article" | grep -E -qi $topic; do
     article=$(wikipedia2text -r)
-    if echo "$article" | grep -E -qi $topic; then
-        success=1
-    fi
-    echo -ne "$(randcolor 150 20).\e[0m"
+    echo -ne "."
 done
-echo
-echo -e "\e[0m$article"
+echo -e "$article"
