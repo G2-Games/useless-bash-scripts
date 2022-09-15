8d65e0031dfed308d483f91df6dae19a377b4edd
G2-Games <ke0bhogsg@gmail.com>
2022-09-15 11:23:20 -0500

    Slimmed things

--- wikipedia-topic.sh
+++ wikipedia-topic.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 topic=$(echo $1 | sed -r 's/[ ,]+/|/g')
-echo $topic
-echo -n "Searching"
+echo "Searching for \"$(echo $topic | sed -r 's/[|]+/, /g')\""
 while ! echo "$article" | grep -E -qi $topic; do
     article=$(wikipedia2text -r)
     echo -ne "."
