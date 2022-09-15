5a38020411cb4b566eead5a2c44139a2246f9c30
G2-Games <ke0bhogsg@gmail.com>
2022-09-15 09:32:04 -0500

    cleaned up a bit

--- wikipedia-topic.sh
+++ wikipedia-topic.sh
@@ -3,14 +3,16 @@
 source randcolor.sh
 
 topic=$1
+topic=$(echo $topic | tr " " '|')
+echo $topic
 echo -n "Searching"
 
 while [[ $success -lt 1 ]]; do
     article=$(wikipedia2text -r)
-    if echo "$article" | grep -qi $topic; then
+    if echo "$article" | grep -E -qi $topic; then
         success=1
     fi
-    echo -ne "$(randcolor 150 20)."
+    echo -ne "$(randcolor 150 20).\e[0m"
 done
 echo
-echo -e "$(randcolor 180 20)$article"
+echo -e "\e[0m$article"
