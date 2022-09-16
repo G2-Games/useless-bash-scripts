2817ad8b306865b30803f1e83a3938e17bfa1866
G2-Games <ke0bhogsg@gmail.com>
2022-09-15 21:08:14 -0500

    More tweaks

--- wikipedia-topic.sh
+++ wikipedia-topic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 topic=$(echo $1 | sed -r 's/[ ,]+/|/g')
+if [[ -z "$1" ]]; then echo -e "Usage: wikipedia-topic \"[TOPICS]...\"" && exit 1; fi
 echo "Searching for \"$(echo $topic | sed -r 's/[|]+/, /g')\""
 while ! echo "$article" | grep -E -qi $topic; do
     article=$(wikipedia2text -r)
